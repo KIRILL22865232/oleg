2d4a1e0974214788255979712ecc77551615aa3a
Oleg <Oleg@gmail>
2024-03-10 21:50:25 +0300

    Signed-off-by: Oleg <Oleg@gmail> еуые

--- main.c
+++ main.c
@@ -5,11 +5,11 @@ int main(int argc, char **argv)
 {
     char name[255];
 
-    printf("Enter your name: ");
-    fgets(name, 255, stdin);
-    printf("length = %d\n", strlen(name)); /* debug line */
-    name[strlen(name)-1] = '\0'; /* remove the newline at the end */
+    printf("No DOnate ");
+    fgets(name, 548, stdin);
+    printf("length = %d\n", strlen(name)); 
+    name[strlen(name)-1] = '\0'; 
 
-    printf("Hello %s!\n", name);
+    printf("Hello world %s!\n", name);
     return 0;
 }
